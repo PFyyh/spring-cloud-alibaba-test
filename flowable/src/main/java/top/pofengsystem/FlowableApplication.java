e7b5d91af6ac17d18134eb468c9bb18798cca893
yyh <1049092041@qq.com>
2021-09-21 15:06:56 +0800

    增加作者名字

--- flowable/src/main/java/top/pofengsystem/FlowableApplication.java
+++ flowable/src/main/java/top/pofengsystem/FlowableApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
+/**
+ * @author YYH
+ */
 @SpringBootApplication
 public class FlowableApplication {
     public static void main(String[] args) {
